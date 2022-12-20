55de3227f8b6743c6e9955024f293bdcb1cae28b
Vladik <vladkostyithenko@gmail.com>
2022-12-21 02:55:06 +0300

    added main file

---
+++ Project_OPI_Lab12/Project_OPI_Lab12/main_file.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int Shifr_Cezyar();
+int main()
+{
+	
+	int choose;
+	
+	cout << " ";
+	
+	cin >> choose;
+	
+	switch (choose) 
+	{
+	case 1:
+	{
+		Shifr_Cezyar();
+	}
+
+	}
+
+}
