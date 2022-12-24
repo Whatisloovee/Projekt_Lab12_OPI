2eddd194df6c7fef3a5c110c14f083afed56ca71
Ksenia <oreshko_2004@mail.ru>
2022-12-24 14:31:39 +0300

    module_12

---
+++ Project_OPI_Lab12/module_12.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void module_12()
+{
+	setlocale(LC_CTYPE, "ru");
+	int sec, min, hour;
+	cout << "Введите часы: ";
+	cin >> hour;
+	cout << endl;
+	cout << "Введите минуты: ";
+	cin >> min;
+	cout << endl;
+	cout << "Введите секунды: ";
+	cin >> sec;
+	while (true)
+	{
+		system("cls");	//Очистка экрана консоли
+		cout << endl << "	  " << "Таймер запущен: ";
+		cout << endl << endl << "	    " << hour << " : " << min << " : " << sec;
+		Sleep(1000);	//задержка на одну секунду
+		if (sec > 0) {
+			sec--;
+		}
+		else if (sec == 0 && min > 0) {
+			min--;
+			sec = 59;	//обновление счётчика секуднд
+		}
+		else if (sec == 0 && min == 0 && hour > 0) {
+			hour--;
+			min = 59;	//обновление счётчика минут и секунд
+			sec = 59;
+		}
+		else if (sec == 0 && min == 0 && hour == 0) {
+			system("cls");
+			cout << endl << endl << "	" << "Время вышло!!!" << endl << endl;
+			break;
+		}
+	}
+}
