9075aab9e19593dea0cb92439122c8bbffd06e5e
Vladik <vladkostyithenko@gmail.com>
2022-12-26 02:16:45 +0300

    Release projekt

--- Project_OPI_Lab12/Модуль Шифр Цезаря.cpp
+++ Project_OPI_Lab12/Модуль Шифр Цезаря.cpp
@@ -12,24 +12,24 @@ int Shifr_Cezyar()
 	char buff[1024];
 	int n, key;
 
-	cout << setw(70) << "\x1B[31mШИФР ЦЕЗАРЯ\033[0m\t\t" << endl << endl;
+	cout << setw(70) << "ШИФР ЦЕЗАРЯ" << endl << endl;
 
-	cout << "\x1B[32mВведите сообщение,которое будет закодированно\033[0m" << endl;
+	cout << "Введите сообщение,которое будет закодированно" << endl;
 	cin.getline(buff, 1024);
-	cout << "\x1B[32mВведите ключ (целое цисло)\033[0m" << endl;
+	cout << "Введите ключ (целое цисло)" << endl;
 	cin >> key;
 	for (int i = 0; i < strlen(buff); i++)
 		buff[i] += key;
-	cout << "\x1B[32mРезультат\033[0m" << endl;
-	printf("\x1B[33m%s\033[0m", buff);
+	cout << "Результат" << endl;
+	printf("%s", buff);
 	cout << endl << endl;
-	cout << "\x1B[32mЕсли хотите выполнить декодировку, то введите \"1\"\033[0m" << endl;
+	cout << "Если хотите выполнить декодировку, то введите \"1\"" << endl;
 	cin >> n;
 	if (n == 1) {
-		cout << "\x1B[32mДекодировка\033[0m" << endl;
+		cout << "Декодировка" << endl;
 		for (int i = 0; i < strlen(buff); i++)
 			buff[i] -= key;
-		printf("\x1B[33m%s\033[0m", buff);
+		printf("%s", buff);
 		cout << endl;
 	}
 	system("pause");
